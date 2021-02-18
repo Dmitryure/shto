45403f145866e53895db7ccc932a82ea523ba3e0
dmitryure <dmitryure@gmail.com>
2021-02-18 23:16:05 +0300

    block moves after mouse

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import { useRef, useState } from 'react'
 import {useSpring} from "react-spring"
-import { useFrame } from 'react-three-fiber'
-import { useDrag } from 'react-use-gesture'
+import { useFrame, useThree } from 'react-three-fiber'
+import { useDrag, useMove } from 'react-use-gesture'
 
 function App(props) {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
-  const [{x,y}, set] = useSpring(() => ({x: 1, y: 1}))
-  const bind = useDrag(({ offset: [x, y] }) => set({ x, y }))
-
+  const {viewport, mouse} = useThree();
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
 
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0
+    if(active) {
+      mesh.current.position.x = (mouse.x * viewport.width) / 2
+      mesh.current.position.y = (mouse.y * viewport.height) / 2
+    }
   })
 
   return (
     <mesh
       {...props}
       ref={mesh}
-      {...bind()}
-      position={[x.value/100, y.value / 100, 0]}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
